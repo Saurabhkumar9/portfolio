7a24cc4061ccf90f55dd3b1815702cbfa0eeb62c
saurabh kumar yadav <165639161+Saurabhkumar9@users.noreply.github.com>
2024-09-19 09:28:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Your message has been sent!');
+
+});
+
+
